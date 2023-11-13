3aa2805bca6fe75f8389f720223ec703a71fa336
tomasdiazkelly <tomasdiazkelly@outlook.com>
2023-11-13 20:48:25 -0300

    que pasa con todo esto

--- src/main.js
+++ src/main.js
@@ -1,41 +1,40 @@
-import express from 'express'
-import { engine } from 'express-handlebars'
-import { Server as IOServer } from 'socket.io'
+import express from 'express';
+import { engine } from 'express-handlebars';
+import { Server } from 'socket.io'; // Cambiado a Server
 
-import { webRouter } from './routers/web.router.js'
-import { apiRouter } from './routers/api.router.js'
-import { productManager } from './services/productManager.js'
+import { webRouter } from './routers/web.router.js';
+import { apiRouter } from './routers/api.router.js';
+import { productManager } from './services/productManager.js';
 
-const app = express()
+const app = express();
 
-app.engine('handlebars', engine())
-app.set('views', './views')
-app.set('view engine', 'handlebars')
-
+app.engine('handlebars', engine());
+app.set('views', './views');
+app.set('view engine', 'handlebars');
 
 const server = app.listen(8080, () => {
-  console.log('Conectado puerto 8080!')
-})
+  console.log('Conectado puerto 8080!');
+});
 
-const ioServer = new IOServer(server)
+const ioServer = new Server(server); // Cambiado a Server
 
-ioServer.on('conexion', async socket => {
-  console.log('nueva conexion: ', socket.id)
-  socket.emit('productos', await productManager.obtenerTodos())
+ioServer.on('connection', async (socket) => {
+  console.log('nueva conexion: ', socket.id);
+  socket.emit('productos', await productManager.obtenerTodos());
 
-  socket.on('nuevoProducto', async producto => {
-    await productManager.agregar(producto)
-    ioServer.sockets.emit('producto', await productManager.obtenerTodos())
-  })
-})
+  socket.on('nuevoProducto', async (producto) => {
+    await productManager.agregar(producto);
+    ioServer.sockets.emit('producto', await productManager.obtenerTodos());
+  });
+});
 
 app.use((req, res, next) => {
-  req['io'] = ioServer
-  next()
-})
+  req['io'] = ioServer;
+  next();
+});
 
-app.use(express.json())
-app.use('/static', express.static('./static'))
+app.use(express.json());
+app.use('/static', express.static('./static'));
 
-app.use('/', webRouter)
-app.use('/api', apiRouter)
+app.use('/', webRouter);
+app.use('/api', apiRouter);
